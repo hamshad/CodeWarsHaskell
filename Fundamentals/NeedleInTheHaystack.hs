7e1b90877a3636128281c11b54711b8816b08729
hamshad <hiromega18@gmail.com>
2025-02-08 23:50:32 +0530

    fix: best practice and clever solution

--- Fundamentals/NeedleInTheHaystack.hs
+++ Fundamentals/NeedleInTheHaystack.hs
@@ -8,3 +8,16 @@ findNeedle haystack = "found the needle at position " ++ show (fromJust (findInd
 
 -- show function converts the integer to a string
 -- findIndex function finds the index of the string "needle" in the list of strings
+-- fromJust function extracts the value from the Maybe type
+
+-- Best practice:
+-- module ANeedleInTheHaystack where
+-- import Data.List (elemIndex)
+-- import Data.Maybe (fromJust)
+-- findNeedle :: [String] -> String
+-- findNeedle = ("found the needle at position " ++) . show . fromJust . elemIndex "needle"
+
+-- Clever solution:
+-- module ANeedleInTheHaystack where
+-- findNeedle :: [String] -> String
+-- findNeedle = ("found the needle at position " ++) . show . length . takeWhile (/= "needle")
